b3400d750e52d89f21c54f2c0f85c36ff0c3c03c
Akshjot Sangha <sanga9@farmingdale.edu>
2021-09-23 16:39:33 -0400

    Product Management first commit

---
+++ ProductManagement/src/com/cogent/productmanagement/Main.java
@@ -0,0 +1,12 @@
+package com.cogent.productmanagement;
+
+import com.cogent.productmanagement.model.Product;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
